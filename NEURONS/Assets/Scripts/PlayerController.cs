740dac9b49d273fde1bf94456d7980ac4b93452d
BREADfox27 <javierluna271107@gmail.com>
2026-01-28 17:14:09 +0100

    PlayerController

--- NEURONS/Assets/Scripts/PlayerController.cs
+++ NEURONS/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D playerRb;
+    private Animator anim;
+    private float horizontalInput;
+
+    public float speed;
+    public float jumpForce;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        playerRb.linearVelocity = new Vector2 (horizontalInput * speed, playerRb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
