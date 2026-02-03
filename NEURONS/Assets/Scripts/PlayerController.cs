3ed011de91b9e856776a70f79462d99314ba1219
BREADfox27 <javierluna271107@gmail.com>
2026-02-03 18:22:19 +0100

    Walking Sound
    José did it
    
    Co-Authored-By: Jossel100 <242467410+Jossel100@users.noreply.github.com>

--- NEURONS/Assets/Scripts/PlayerController.cs
+++ NEURONS/Assets/Scripts/PlayerController.cs
@@ -199,9 +199,4 @@ public class PlayerController : MonoBehaviour
         transform.localScale = currentScale;
         isFacingRight = !isFacingRight;
     }
-
-    public void WalkingSound()
-    {
-        AudioManager.Instance.PlaySFX(1);
-    }
 }
